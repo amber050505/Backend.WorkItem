e360b2d2ec82339c376a76d20b1f59f1c08136ff
pohsuan <yellow1230605@gmail.com>
2025-10-26 22:46:02 +0800

    新增單元測試: 測試 Service 正確回應 Repository 傳回的新資料 id

---
+++ Backend.WorkItem/Backend.WorkItem.Test/ServiceReturnIdTest.cs
@@ -0,0 +1,29 @@
+using Backend.WorkItem.Repository.WorkItem.Interface;
+using Backend.WorkItem.Service.WorkItem;
+using Moq;
+
+namespace Backend.WorkItem.Test
+{
+    public class WorkItemServiceTests
+    {
+        private Mock<IWorkItemRepository> _mockRepo;
+        private WorkItemService _service;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockRepo = new Mock<IWorkItemRepository>();
+            _service = new WorkItemService(_mockRepo.Object);
+        }
+
+        [Test]
+        public async Task Test1()
+        {
+            var workItem = new Model.WorkItem { Title = "Test Title" };
+            _mockRepo.Setup(repo => repo.CreateAsync(It.IsAny<Model.WorkItem>())).ReturnsAsync(100);
+
+            var workItemId = await _service.CreateAsync(workItem);
+            Assert.That(workItemId, Is.EqualTo(100));
+        }
+    }
+}
