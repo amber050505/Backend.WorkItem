e21e9562706ec052d26380b1b7529e30e8435cdb
pohsuan <yellow1230605@gmail.com>
2025-10-26 12:43:31 +0800

    新增專案

---
+++ Backend.WorkItem/Backend.WorkItem.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Backend.WorkItem.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Backend.WorkItem/Backend.WorkItem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Backend.WorkItem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
