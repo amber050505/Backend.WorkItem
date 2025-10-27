579bbbad6dd2528fb8d6d81baa146fba697e5246
pohsuan <yellow1230605@gmail.com>
2025-10-27 21:21:27 +0800

    加入 Swagger

--- Backend.WorkItem/Backend.WorkItem/Program.cs
+++ Backend.WorkItem/Backend.WorkItem/Program.cs
@@ -15,6 +15,8 @@ namespace Backend.WorkItem
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
             builder.Services.AddScoped<IWorkItemRepository, WorkItemRepository>();
             builder.Services.AddScoped<IWorkItemService, WorkItemService>();
             builder.Services.AddScoped<IConnectionString, ConnectionString>();
@@ -22,7 +24,12 @@ namespace Backend.WorkItem
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            else
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
