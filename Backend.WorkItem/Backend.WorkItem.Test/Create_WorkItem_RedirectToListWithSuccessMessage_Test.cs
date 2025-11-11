18a3b47d7af8eda4432e555fcd086d187d8ad859
pohsuan <yellow1230605@gmail.com>
2025-11-11 23:00:05 +0800

    調整單元測試的 Controller 名稱

--- Backend.WorkItem/Backend.WorkItem.Test/Create_WorkItem_RedirectToListWithSuccessMessage_Test.cs
+++ Backend.WorkItem/Backend.WorkItem.Test/Create_WorkItem_RedirectToListWithSuccessMessage_Test.cs
@@ -29,10 +29,10 @@ namespace Backend.WorkItem.Test
             var model = new Model.WorkItem { Title = "Test Title" };
             _mockService.Setup(service => service.CreateAsync(It.Is<Model.WorkItem>(item => item.Title != "")));
 
-            var result = await _controller.Create(model) as RedirectToActionResult;
+            var result = await _controller.New(model) as RedirectToActionResult;
 
             Assert.That(result, Is.Not.Null);
-            Assert.That(result!.ActionName, Is.EqualTo("List"));
+            Assert.That(result!.ActionName, Is.EqualTo(nameof(_controller.List)));
             Assert.That(_controller.TempData["SuccessMessage"], Is.EqualTo("已送出新增請求（由背景服務寫入）"));
         }
 
